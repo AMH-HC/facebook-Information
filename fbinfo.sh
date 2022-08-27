b3295f6a0ef3e72fc1b71ae1da049e226899ea30
AMH-HC <89603191+AMH-HC@users.noreply.github.com>
2022-08-27 22:40:27 +0630

    Update fbinfo.sh

--- fbinfo.sh
+++ fbinfo.sh
@@ -16,13 +16,13 @@ command
 about () {
 echo -e "\e[1;32m
 {}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}
-{}Name                          Yell Phone Naing{}
-{}Age                                    17 Year{}
-{}Fb               https://fb.me/yellphoen.naing{}
+{}Name                              Thet Htet Ko{}
+{}Age                                    23 Year{}
+{}Fb               https://fb.me/100060803555433{}
 {}Skills             HTML,CSS,ZSH,PHP,MYSQL,BASH{}
-{}Mail                  yellphonenaing@gmail.com{}
+{}Mail                         hackcat@gmail.com{}
 {}     You Can Contact Me On FB For Any Help    {}
-{}{}{}{}{}{}{}||Date::7/7/2020||{}{}{}{}{}{}{}{}{}
+{}{}{}{}{}{}{}||Date::27/8/2022||{}{}{}{}{}{}{}{}{}
 {}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}
 \e[0m"
 command
@@ -75,7 +75,7 @@ echo -e "\e[1;31m
 {}       ==[ Note:To Harm Something             {}
 {}       ==[ Note:Hack Everything,Harm Nothing  {}
 {}       ==[ Tool Name : FB ACC Info Collector  {}
-{}       ==[ Coded by : Yell Phone Naing        {}
+{}       ==[ Coded by : Hack Cat                {}
 {}       ==[ Version : 1.0.0                    {}
 {}       ==[ Script Types :Shell,PHP            {}
 {}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}
